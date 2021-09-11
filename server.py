9b22a80017c0a36fb8ce59166917da70ec42da6c
Matthew Donnelly <brilliantrocketscientist@outlook.com>
2021-09-11 09:56:06 -0700

    Chapter 8

--- server.py
+++ server.py
@@ -59,4 +59,37 @@ def bands():
 
     return jsonify(success=True, bands=bands)
 
+
+@app.route('/v1/songs')
+def songs():
+    '''
+    Returns a list of songs.
+    '''
+
+    songs = [
+        {
+            'title': 'Black Dog',
+            'band': 'led-zeppelin',
+            'rating': 3,
+        },
+        {
+            'title': 'Yellow Ledbetter',
+            'band': 'pearl-jam',
+            'rating': 4,
+        },
+        {
+            'title': 'The Pretender',
+            'band': 'foo-fighters',
+            'rating': 2,
+        },
+        {
+            'title': 'Daughter',
+            'band': 'pearl-jam',
+            'rating': 5,
+        }
+    ]
+
+    return jsonify(success=True, songs=songs)
+
+
 app.run(host='0.0.0.0')
