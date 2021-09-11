d9761894c59c3bea231d3f88479ee6ace0d13e07
Matthew Donnelly <brilliantrocketscientist@outlook.com>
2021-09-11 10:45:14 -0700

    Revise API

--- server.py
+++ server.py
@@ -15,45 +15,18 @@ def bands():
     Returns a list of bands.
     '''
 
-    blackDog = {
-        'title': 'Black Dog',
-        'band': 'Led Zeppelin',
-        'rating': 3,
-    }
-
-    yellowLedbetter = {
-        'title': 'Yellow Ledbetter',
-        'band': 'Pearl Jam',
-        'rating': 4,
-    }
-
-    pretender = {
-        'title': 'The Pretender',
-        'band': 'Foo Fighters',
-        'rating': 2,
-    }
-
-    daughter = {
-        'title': 'Daughter',
-        'band': 'Pearl Jam',
-        'rating': 5,
-    }
-
     bands = [
         {
           'id': 'led-zeppelin',
           'name': 'Led Zeppelin',
-          'songs': [blackDog],
         },
         {
           'id': 'pearl-jam',
           'name': 'Pearl Jam',
-          'songs': [yellowLedbetter, daughter],
         },
         {
           'id': 'foo-fighters',
           'name': 'Foo Fighters',
-          'songs': [pretender],
         },
     ]
 
